3236b71f113e88180d14f1e912bfa0a0a0a95c41
25383 <lingyu.xu@hand-china.com>
2019-08-12 10:36:58 +0800

    header

---
+++ vue-travel/Travel/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <HomeHeader></HomeHeader>
+ <div>home</div>
+<router-link to="/list">列表页</router-link>
+</div>
+</template>
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components:{
+    HomeHeader
+  }
+}
+</script>
+<style>
+
+</style>
